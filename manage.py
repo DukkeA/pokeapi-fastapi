f7ce44a0ea5e8541668d48b80646592b805e8523
Juan Motta <juanandres.lopez@crehana.com>
2023-12-10 14:00:48 -0500

    chore: added migrate and makemigrations commands to manage.py

--- manage.py
+++ manage.py
@@ -5,8 +5,22 @@ app = typer.Typer()
 
 
 @app.command()
-def runserver(name: str):
+def runserver():
     uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)  # type: ignore
+
+
+@app.command()
+def makemigrations():
+    from subprocess import run
+
+    run(["alembic", "revision", "--autogenerate", "-m", "init"], check=True)
+
+
+@app.command()
+def migrate():
+    from subprocess import run
+
+    run(["alembic", "upgrade", "head"], check=True)
 
 
 if __name__ == "__main__":
